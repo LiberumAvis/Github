e465352aff36add0ab3f4df54e7240d33766e9af
Dreamer <julia12pet@gmail.com>
2017-10-08 11:24:57 +0300

    Start project GITHUBproject

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+
+	public class ClassInt{
+	    private int i;
+	    private String str;
+        {
+            i=123456789;
+            str="Bello";
+        }
+    }
+    public static void main(String[] args) {
+    }
+}
